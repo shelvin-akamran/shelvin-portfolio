80622b4532aeef4b400f9c49dc89d96c132bf4b1
user.name <akamrannicole@gmail.com>
2026-02-15 15:53:15 +0300

    rename Navigation and HeroSection files to PascalCase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import Navigation from "./components/navigation.jsx"
-import Hero from "./components/hero-section.jsx"
+import Navigation from "./components/Navigation.jsx"
+import Hero from "./components/HeroSection.jsx"
 
 const MainLayout = () => {
   return (
