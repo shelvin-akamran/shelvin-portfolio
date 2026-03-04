b7dc645014653b38c964f77854d79bd919c20a88
user.name <akamranshelvin@gmail.com>
2026-03-04 22:24:20 +0300

    Add WorkPage route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import CTASection from './components/CTASection';
 // Pages
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import WorkPage from './pages/WorkPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about-page" element={<AboutPage />} />
+        <Route path="/work" element={<WorkPage />} />
       </Routes>
 
       <SayHello/>
